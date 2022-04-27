fb06a3fa284d87bcaad4bf41ba5312237cd7bdb7
bonuanil <bonuanil.june@gmail.com>
2022-04-26 20:19:43 -0400

    Initial Version

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Title
+
+Class Enrollment System
+
+## Description
+
+Develop a basic Class Enrollment System for Institutions & Students
+
+## Getting Started
+
+### Assumptions
+
+
+### API Details
+
+## Authors
+1. Anil Kumar Bonu
---
+++ src/main/java/com/ces/ces/CesApplication.java
@@ -0,0 +1,13 @@
+package com.ces.ces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesApplication.class, args);
+	}
+
+}
